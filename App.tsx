6ff6d89d97f7a0007bbe3b86c22acd168f681026
Gabriel Castro <gabrieldcc@gmail.com>
2025-03-29 08:48:22 -0300

    adjusts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
+// import HomeScreen from "./src/screens/HomeScreen";
+// import DetailsScreen from "./src/screens/DetailsScreen";
 
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen 
+//           name="Main" 
+//           component={MainTabs} 
+//           options={{ headerShown: false }} 
+//         />
+//         <Stack.Screen 
+//           name="Details" 
+//           component={DetailsScreen} 
+//           options={{ headerShown: true }} 
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// function MainTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen 
+//         name="HomeStack" 
+//         component={HomeStack} 
+//         options={{ title: "Home", headerShown: false }} 
+//       />
+//       <Tab.Screen name="Details" component={DetailsScreen} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// function HomeStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen 
+//         name="Home" 
+//         component={HomeScreen} 
+//         options={{ headerShown: true }} 
+//       />
+//       <Stack.Screen 
+//         name="Details" 
+//         component={DetailsScreen} 
+//         options={{ tabBarStyle: { display: "none" } }} 
+//       />
+//     </Stack.Navigator>
+//   );
+// }
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();  
+
+const linking = {
+  prefixes: ['myapp://'], 
+  config: {
+    screens: {
+      Home: '',
+      Details: 'details/:itemId', 
+    },
+  },
+};
+
+
+// Função principal do App
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer linking={linking}>
+      <Drawer.Navigator initialRouteName="HomeTabs">
+        <Drawer.Screen name="HomeTabs" component={HomeTabs} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Navegação das Tabs
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Details" component={DetailsScreen} options={{ tabBarVisible: false }} />
+    </Tab.Navigator>
+  );
+}
+
+// Navegação Stack dentro da Home
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen 
+        name="Details" 
+        component={DetailsScreen} 
+        options={{ 
+          title: "Detalhes",
+          headerShown: true, 
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
